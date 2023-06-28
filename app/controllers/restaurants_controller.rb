ac0bd1010547b7d2ca62ecfb8c1a6c3a34a6ebae
William Mtemi Mponzi <willbrian009@gmail.com>
2023-06-28 22:20:16 +0300

    route action creation

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,27 @@
 class RestaurantsController < ApplicationController
+
+    def index
+        restaurants = Restaurant.all
+        render json: restaurants, only: [:id, :name, :address]
+    end
+
+    def show
+        restaurant = Restaurant.find_by(id: params[:id])
+        if restaurant
+            render json: restaurant, only: [:id, :name], include: { pizzas: { only: [:id, :name, :ingredients] } }
+        else
+            render json: { error: 'Restaurant not found' }, status: :not_found
+        end
+    end
+
+    def destroy
+        restaurant = Restaurant.find_by(id: params[:id])
+        if restaurant
+          restaurant.destroy
+          head :no_content
+        else
+          render json: { error: 'Restaurant not found' }, status: :not_found
+        end
+      end
+
 end
